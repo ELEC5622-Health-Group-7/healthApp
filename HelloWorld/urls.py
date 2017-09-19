067b48bdb723ba6625bdf7bfdeb38d46ff6bfddb
Newman666 <tian241033@gmail.com>
2017-09-19 11:51:45 +1000

    1

---
+++ HelloWorld/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import *
+from . import view,testdb
+ 
+urlpatterns = [
+    url(r'^hello$', view.hello),
+    url(r'^calendar$', view.calendar),
+    url(r'^testdb$', testdb.testdb),
+]
